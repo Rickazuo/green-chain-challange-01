7fdb3866c248c989bb5e8bbfa9481932aa444bb5
Rickazuo <rick3209@gmail.com>
2023-03-18 17:21:20 -0300

    create mobile style

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-import './style.css'
+// import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div class="container">
-  
-  </div>
-`
+const menuBtn = document.querySelector('.button-sandwich')
+const mobileMenuBtn = document.querySelector('.button-mobile')
+const expandMenu = document.querySelector('.main-square')
+const expandMenuMobile = document.querySelector('.header-mobile')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open')
+    expandMenu.classList.add('open')
+    menuOpen = true
+  } else {
+    menuBtn.classList.remove('open')
+    expandMenu.classList.remove('open')
+    menuOpen = false
+  }
+})
+
+mobileMenuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    mobileMenuBtn.classList.add('open')
+    expandMenuMobile.classList.add('open')
+    menuOpen = true
+  } else {
+    mobileMenuBtn.classList.remove('open')
+    expandMenuMobile.classList.remove('open')
+    menuOpen = false
+  }
+})
 
