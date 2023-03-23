61698bb12473acf058b8c2db223065c317e3577c
Rickazuo <rick3209@gmail.com>
2023-03-23 20:55:33 -0300

    update sprint 02 land page

--- main.js
+++ main.js
@@ -4,17 +4,6 @@ const mobileMenuBtn = document.querySelector('.button-mobile')
 const expandMenu = document.querySelector('.main-square')
 const expandMenuMobile = document.querySelector('.header-mobile')
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open')
-    expandMenu.classList.add('open')
-    menuOpen = true
-  } else {
-    menuBtn.classList.remove('open')
-    expandMenu.classList.remove('open')
-    menuOpen = false
-  }
-})
 
 mobileMenuBtn.addEventListener('click', () => {
   if(!menuOpen) {
